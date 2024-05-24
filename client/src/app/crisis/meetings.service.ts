b7f012a58aec235df7714533f2708249d2856f48
gaetan <gaetan.yvert@institutsolacroup.com>
2024-05-24 09:57:58 +0200

    meetings

--- client/src/app/crisis/meetings.service.ts
+++ client/src/app/crisis/meetings.service.ts
@@ -17,4 +17,24 @@ export class MeetingsService {
     // return this.crisislist;
     return this.http.get<meetingsmodel[]>(this.link);
   }
+
+  getMeetingByCrisisId(crisisId: number): Observable<meetingsmodel[]> {
+    const url = `${this.link}?filter[where][crisisId]=${crisisId}`;
+    return this.http.get<meetingsmodel[]>(url);
+  }
+
+  addMeeting(meeting: meetingsmodel): Observable<meetingsmodel> {
+    return this.http.post<meetingsmodel>(this.link, meeting);
+  }
+
+  deleteMeeting(id: number): Observable<any> {
+    return this.http.delete<meetingsmodel>(this.link + `/${id}`);
+  }
+
+  updateMeeting(meeting: meetingsmodel): Observable<any> {
+    console.log('MEEEEEEEEEEEEEEEETTTINNNNG:', meeting, 'IDDDDDDDDDDDDD', meeting.id)
+    return this.http.patch(this.link + `/${meeting.id}`, meeting)
+  }
+
 }
+
